0cebd6c1020f03b36e68e9827972f7365103c03b
KManiKumarReddy <KManiKumarReddy@live.com>
2015-11-16 00:24:12 +0530

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -16,6 +16,43 @@ NOTES: If there are no common words return NULL.
 
 #define SIZE 31
 
-char ** commonWords(char *str1, char *str2) {
+char * wordExists(char * word, int begin, int end, char * text) {
+	int i = 0, j;
+	while (text[i] && text[i] == ' ') i++;
+	while (text[i]) {
+		for (j = begin; j <= end && word[j] == text[i] && text[i]; j++, i++);
+		if (j == end + 1 && (text[i] == ' ' || !text[i])) {
+			// copying word to a new string and returning it
+			char *string = (char *)malloc(sizeof(char)* SIZE);
+			string[end - begin + 1] = 0;
+			while (begin <= end)
+				string[(end--) - begin] = word[end];
+			return string;
+		}
+		if (text[i] == ' ') {
+			while (text[i] && text[i] == ' ') i++;
+		}
+		else {
+			while (text[i] && text[i] != ' ') i++;
+			while (text[i] && text[i] == ' ') i++;
+		}
+	}
 	return NULL;
+}
+
+
+char ** commonWords(char *str1, char *str2) {
+	if (!str1 || !str1 || !(*str1) || !(*str2))
+		return NULL;
+	int i = -1, wordBegin, count = 0;
+	char **result = (char **)calloc(SIZE, sizeof(char*)), *temp;
+	while (str1[++i]) {
+		if (str1[i] != ' ') {
+			wordBegin = i;
+			while (str1[++i] && str1[i] != ' ');
+			if (temp = wordExists(str1, wordBegin, i - 1, str2))
+				*(result + count++) = temp;
+		}
+	}
+	return count > 0 ? result : NULL;
 }
